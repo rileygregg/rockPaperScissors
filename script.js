05b01b50add5f7584c3ec2cd9b74c8e3b588fc0f
Riley Gregg <rtgregg93@gmail.com>
2022-05-30 16:09:29 -0400

    Sudo code started in .js file

---
+++ script.js
@@ -0,0 +1,13 @@
+// Rock paper Scissors
+
+// When a user chooses a Symbol
+// let the computer choose a random Symbol 
+// If the user chooses Rock and the computer chooses Scissors then print "Win"
+    // else if the computer chooses Rock then print "Tie"
+    // else print "Lose"
+// If the user chooses Paper and the computer chooses Rock then print "Win"
+    // else if the computer chooses Paper then print "Tie"
+    // else print "Lose"
+// If the user chooses Scissors and the computer chooses paper then print "win"
+    // else if the computer chooses Scissors then print "Tie"
+    // else print "Lose"
